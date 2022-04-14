f7c67af20aaa4a0eaa0515b0ad7a6e165355de43
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-14 18:24:57 +0100

    innitialised project

---
+++ AdobeClone/src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Slider from './components/Slider'
+
+const App = () => {
+  return (
+    <div className='container' >
+      <div className="main-image"></div>
+      <div className="sidebar">
+
+      </div>
+      <Slider/>
+    </div>
+  )
+}
+
+export default App
---
+++ AdobeClone/src/components/Slider.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div className='slider-container' >
+      <input type="range" name="" id="" className='slider' />
+    </div>
+  )
+}
+
+export default Slider
